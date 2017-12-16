c9969f3a06d0a218dea11f0e948de89307ea1e6a
hemanthkumar.lavu <hemanthkumar.lavu@outlook.com>
2017-12-16 21:34:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,87 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
+var artical = {
+  'artical-one':{
+      title:'Articat | one',
+      hedding:'My artical one',
+      content:`
+    <p>         This is the first artical useing the node js. This is the first artical useing the node js.
+              This is the first artical useing the node js. This is the first artical useing the node js. This is the first artical useing the node js.
+              This is the first artical useing the node js. This is the first artical useing the node js.
+    </p>
+    <p>    This is the first artical useing the node js. This is the first artical useing the node js.
+            This is the first artical useing the node js. This is the first artical useing the node js. This is the first artical useing the node js.
+          This is the first artical useing the node js. This is the first artical useing the node js.
+    </p>
+    <p>     This is the first artical useing the node js. This is the first artical useing the node js.
+              This is the first artical useing the node js. This is the first artical useing the node js. This is the first artical useing the node js.
+              This is the first artical useing the node js. This is the first artical useing the node js.
+      </p>`
+  },
+  'artical-two':{
+      title:'Articat | two',
+      hedding:'My artical two',
+      content:`<p>         This is the first artical useing the node js. This is the first artical useing the node js.
+              This is the first artical useing the node js. This is the first artical useing the node js. This is the first artical useing the node js.
+              This is the first artical useing the node js. This is the first artical useing the node js.
+    </p>
+    <p>    This is the first artical useing the node js. This is the first artical useing the node js.
+            This is the first artical useing the node js. This is the first artical useing the node js. This is the first artical useing the node js.
+          This is the first artical useing the node js. This is the first artical useing the node js.
+    </p>
+    <p>     This is the first artical useing the node js. This is the first artical useing the node js.
+              This is the first artical useing the node js. This is the first artical useing the node js. This is the first artical useing the node js.
+              This is the first artical useing the node js. This is the first artical useing the node js.
+      </p>`
+  },
+  'artical-three':{
+      title:'Articat | threee',
+      hedding:'My artical three',
+      content:`<p>         This is the first artical useing the node js. This is the first artical useing the node js.
+              This is the first artical useing the node js. This is the first artical useing the node js. This is the first artical useing the node js.
+              This is the first artical useing the node js. This is the first artical useing the node js.
+    </p>
+    <p>    This is the first artical useing the node js. This is the first artical useing the node js.
+            This is the first artical useing the node js. This is the first artical useing the node js. This is the first artical useing the node js.
+          This is the first artical useing the node js. This is the first artical useing the node js.
+    </p>
+    <p>     This is the first artical useing the node js. This is the first artical useing the node js.
+              This is the first artical useing the node js. This is the first artical useing the node js. This is the first artical useing the node js.
+              This is the first artical useing the node js. This is the first artical useing the node js.
+      </p>`
+  }
+};
+function contentHtml(data){
+    var title = data.title;
+    var hedding = data.hedding;
+    var content = data.content;
+    var htmlTemplet = `
+            <html>
+            <head>
+            <title>${title}</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" type="text/css" href="ui/style.css" >
+            </head>
+            <body classs="my-body">
+            <h1>${hedding}</h1>
+            <hr><br>
+            <div class= "metiral">
+            ${content}
+            </div>
+            </body>
+            </html>
+            
+            `;
+            return htmlTemplet;
+}
+
+app.get('/:articalName', function(req, res){
+    var articalName = req.params.articalName;
+    res.send(contentHtml(artical[articalName]));
+});
 app.get('/ui/password', function (req, res){
    res.sendFile(path.join(__dirname, 'ui', 'password.html'));
 });
